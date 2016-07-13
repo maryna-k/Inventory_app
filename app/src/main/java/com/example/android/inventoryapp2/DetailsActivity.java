859725336f0777167fde155aa568013d4081b3a6
Maryna Kalachova <kalachova.maryna@gmail.com>
2016-07-12 23:52:51 -0400

    Add methods to order the product from the DetailsActivity.
    Order button opens an intent that sends an email to the supplier.

--- app/src/main/java/com/example/android/inventoryapp2/DetailsActivity.java
+++ app/src/main/java/com/example/android/inventoryapp2/DetailsActivity.java
@@ -1,6 +1,8 @@
 package com.example.android.inventoryapp2;
 
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,6 +15,8 @@ public class DetailsActivity extends AppCompatActivity implements Observer{
     Cursor cursor;
     DBHelper helper;
     String name;
+    String supplier;
+    String supplierEmail;
     int qty;
     InventoryCursorAdapter itemAdapter;
 
@@ -27,7 +31,6 @@ public class DetailsActivity extends AppCompatActivity implements Observer{
 
         Bundle extras = getIntent().getExtras();
         name = extras.getString("name_to_display");
-        Log.v("Name of item: ", name);
 
         cursor = helper.getOneProduct(name);
 
@@ -39,14 +42,14 @@ public class DetailsActivity extends AppCompatActivity implements Observer{
 
         int price = cursor.getInt(cursor.getColumnIndexOrThrow("price"));
         qty = cursor.getInt(cursor.getColumnIndexOrThrow("quantity"));
-        String supplier = cursor.getString(cursor.getColumnIndexOrThrow("supplier_name"));
-        String email = cursor.getString(cursor.getColumnIndexOrThrow("email"));
+        supplier = cursor.getString(cursor.getColumnIndexOrThrow("supplier_name"));
+        supplierEmail = cursor.getString(cursor.getColumnIndexOrThrow("email"));
 
         itemName.setText("Product: " + name);
         itemPrice.setText("Price: " + String.valueOf(price) + "$");
         itemQty.setText("Quantity: " + String.valueOf(qty));
         itemSupplier.setText("Supplier: " + supplier);
-        itemEmail.setText("Email: " + email);
+        itemEmail.setText("Email: " + supplierEmail);
 
         Button sold = (Button) findViewById(R.id.button_sold);
         sold.setOnClickListener(handleSold);
@@ -84,7 +87,12 @@ public class DetailsActivity extends AppCompatActivity implements Observer{
 
     View.OnClickListener handleOrder = new View.OnClickListener() {
         public void onClick(View v) {
-
+            int qtyToOrder = quantityToOrder();
+            String orderMessage = composeOrderMessage(supplier, qtyToOrder, name);
+            Log.v("Order message: ", orderMessage);
+            String subject = "Order of " + name;
+            Log.v("Order subject: ", subject);
+            composeEmail(supplierEmail,subject,orderMessage);
         }
     };
 
@@ -94,6 +102,7 @@ public class DetailsActivity extends AppCompatActivity implements Observer{
         }
     };
 
+    //gets number sold from EditText
     public int quantitySold () {
         EditText number = (EditText) findViewById(R.id.enterNumberSold);
         try {
@@ -105,6 +114,7 @@ public class DetailsActivity extends AppCompatActivity implements Observer{
         return 0;
     }
 
+    //gets number received from EditText
     public int quantityReceived () {
         EditText number = (EditText) findViewById(R.id.enterNumberReceived);
         try {
@@ -114,6 +124,39 @@ public class DetailsActivity extends AppCompatActivity implements Observer{
             Log.v("Quantity received:", "Could not parse quantity received");
         }
         return 0;
+    }
+
+    //gets number to order from EditText
+    public int quantityToOrder() {
+        EditText qtyToOrder = (EditText) findViewById(R.id.enterNumberToOrder);
+        try {
+            int qty = Integer.parseInt(qtyToOrder.getText().toString());
+            Log.v("Order quantity: ", String.valueOf(qty));
+            return qty;
+        } catch(NumberFormatException e) {
+            Log.v("Parse quantity order", "Could not parse quantity to order");
+        }
+        return 0;
+    }
+
+    private String composeOrderMessage(String supplierName, int quantity, String productName) {
+
+        String orderMessage = "Dear " + supplierName + ",\n";
+        orderMessage = orderMessage + "Our store would like to order " + quantity + " " + productName + ".\n";
+        orderMessage = orderMessage + "Sincerely,\n" + "Store";
+        return orderMessage;
+    }
+
+    //implicit intent to sent an order email
+    public void composeEmail(String supplierEmail, String subject, String orderMessage) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); //only email apps should handle this intent
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[] {supplierEmail});
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, orderMessage);
+        if (intent.resolveActivity(getPackageManager())!= null) {
+            startActivity(intent);
+        }
     }
 
     @Override
